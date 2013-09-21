fb2a58b040837220de515402ecb99acdee49222f
hayk <hamshen@gmail.com>
2013-09-22 03:36:43 +0400

    Update phpdoc

--- ylh/ylh.php
+++ ylh/ylh.php
@@ -2,9 +2,12 @@
 /**
  *
  * YouTube Like Hash v0.2
- * Copyright (c) 2006 Hayk Chamyan <hamshen@gmail.com>
- * https://github.com/hayk/xpets
- * GNU General Public License version 2
+ *
+ * @author		Hayk Chamyan <hamshen@gmail.com>
+ * @version		0.2
+ * @copyright	2006-2013 Hayk Chamyan
+ * @link		https://github.com/hayk/xpets
+ * @license		GNU General Public License version 2
  */
 
 function get_ylhash($data=null, $long=false)
@@ -15,20 +18,20 @@ function get_ylhash($data=null, $long=false)
 		'/' => '_',
 		'=' => '',
 	);
- 
+
 	if (is_null($data))
 	{
 		$data = crypt( mt_rand().uniqid() );
 	}
- 
+
 	$data = md5($data, true);
- 
+
 	if(!$long)
 	{
 		$data = substr($data, 0, 8);
 	}
- 
+
 	$data = strtr(base64_encode($data), $trans);
- 
+
 	return $data;
 }
