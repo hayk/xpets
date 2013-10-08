4a5cd6e0d1114958ab6b55457477e20bc7c9589e
hayk <hamshen@gmail.com>
2013-10-08 14:50:51 -0700

    Update closed.php

--- closed/closed.php
+++ closed/closed.php
@@ -9,11 +9,11 @@ header('Retry-After: '.gmdate(DATE_RFC822, $online));
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title>example.com: ведутся технические работы</title>
+	<title>example.com: site on maintenance</title>
 </head>
 <body>
 	<div style="text-align: center; width: 300px; height: 200px; position: absolute; top: 50%; left: 50%; margin-top: -100px; margin-left: -150px;">
-		<h2>На сайте ведутся технические работы. Приблизительное время включения <?php echo date('H:i d.m.Y', $online); ?>.</h2>
+		<h2>Site on maintenance. It will be available at <?php echo date('H:i d.m.Y', $online); ?>.</h2>
 	</div>
 </body>
 </html>
